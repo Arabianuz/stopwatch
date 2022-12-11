813c2a53996fd4a9ebb8917507cd58ee4417711c
Arabianuz <shermuhammadubaydillayev@gmail.com>
2022-12-11 15:24:10 +0500

    some changes

--- stopwatch.js
+++ stopwatch.js
@@ -12,15 +12,28 @@ let scount = 0;
 
 startBtn.addEventListener('click', function() {
     conditionbtn = true;
+    stopBtn.removeAttribute('disabled');
+    startBtn.setAttribute('disabled', '');
+    if (startBtn.innerText == 'Resume') {
+        resetBtn.setAttribute('disabled', '');
+    }
     });
 
-stopBtn.addEventListener('click', function() {conditionbtn = false;});
+stopBtn.addEventListener('click', function() {
+    conditionbtn = false;
+    resetBtn.removeAttribute('disabled');
+    startBtn.removeAttribute('disabled');
+    startBtn.innerText = 'Resume';
+    });
 resetBtn.addEventListener('click', function(){
+    startBtn.innerText = 'Start';
+    stopBtn.setAttribute('disabled', '');
+    resetBtn.setAttribute('disabled', '');
     conditionbtn = false;
     mscount = 0;
     scount = 0;
-    seconds = '00'
-    milli_second = '000';
+    seconds = '00<strong>s:</strong>';
+    milli_second = '000<strong>ms</strong>';
     document.getElementById('milli_seconds').innerHTML = milli_second;
     document.getElementById('seconds').innerHTML = seconds;
 });
@@ -33,34 +46,34 @@ function start() {
    if (conditionbtn) {
        mscount++;
        if (mscount < 10){
-          milli_second = '00' + mscount;
+          milli_second = '00' + mscount + '<strong>ms</strong>';
           document.getElementById('milli_seconds').innerHTML = milli_second;
        }
        else if (mscount < 100 && mscount >= 10) {
-        milli_second = '0'+ mscount;
+        milli_second = '0'+ mscount + '<strong>ms</strong>';
         document.getElementById('milli_seconds').innerHTML = milli_second;
        }
        else {
-        milli_second = mscount;
+        milli_second = mscount + '<strong>ms</strong>';
         document.getElementById('milli_seconds').innerHTML = milli_second;
        }
        if (mscount==1000) {
         scount++;
         mscount = 0;
-        milli_second = '000';
+        milli_second = '000<strong>ms</strong>';
         document.getElementById('milli_seconds').innerHTML = milli_second;
        }
        if (scount < 10){
-        seconds = '0' + scount + ',';
+        seconds = '0' + scount + '<strong>s:</strong>';
         document.getElementById('seconds').innerHTML = seconds;
        }
        else if (scount < 60 && scount >= 10){
-        seconds = scount + ',';
+        seconds = scount + '<strong>s:</strong>';
         document.getElementById('seconds').innerHTML = seconds;
        }
        else {
         scount = 0;
-        seconds = '00,';
+        seconds = '00<strong>s:</strong>';
         document.getElementById('seconds').innerHTML = seconds;
        }
 
